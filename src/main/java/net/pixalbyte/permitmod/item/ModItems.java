b4b0d94b724991794a163cd78166cc84c02a7804
Pixal Byte <dev.pixalbyt3@gmail.com>
2025-05-27 08:03:16 +0300

    Registered the ModItems class and added the permit item. Also added it to the Ingridients tab

--- src/main/java/net/pixalbyte/permitmod/item/ModItems.java
+++ src/main/java/net/pixalbyte/permitmod/item/ModItems.java
@@ -4,12 +4,15 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.pixalbyte.permitmod.PermitMod;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, PermitMod.MOD_ID);
 
+    public static final RegistryObject<Item> PERMIT = ITEMS.register("permit",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
